c629f028beacb8c15212eee3fc623996e75bd438
Joaquin Arias <joaquinarias989@gmail.com>
2022-05-28 11:10:21 -0300

    Agregado Bootstrap. Migrado parte del css del Proyecto. Agregado el NavBar Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>SreetWear - React APP</h2>
-      </header>
-    </div>
-  );
+  return <NavBar />;
 }
 
 export default App;
