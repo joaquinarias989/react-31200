fb708be28bafba8990e8d7ffbd534da658a19484
Joaquin Arias <joaquinarias989@gmail.com>
2022-06-06 18:36:52 -0300

    Corregidos algunos detalles en el ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -7,16 +7,18 @@ const ItemListContainer = ({ greeting }) => {
       <h1 className="title-primary mb-5">{greeting}</h1>
 
       <article className="product__card">
-        <div className="product__card__img">
-          <img src={shirt} alt=""></img>
-        </div>
-        <div className="product__card__info">
-          <h4 className="product__card__title">Producto de Ejemplo</h4>
-          <h4 className="product__card__price">$ 2380</h4>
-          <div className="product__card__btn flex-column gap-3">
-            <ItemCount stock={5} initial={0} />
+        <a href>
+          <div className="product__card__img">
+            <img src={shirt} alt=""></img>
           </div>
-        </div>
+          <div className="product__card__info">
+            <h4 className="product__card__title">Producto de Ejemplo</h4>
+            <h4 className="product__card__price">$ 2380</h4>
+            <div className="product__card__btn flex-column gap-3">
+              <ItemCount stock={5} initial={0} />
+            </div>
+          </div>
+        </a>
       </article>
     </section>
   );
