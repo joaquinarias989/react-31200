0c196ac439cc79f60db6457d0293f81976b94563
Joaquin Arias <joaquinarias989@gmail.com>
2022-06-09 17:17:17 -0300

    Corregido un error de merge

--- src/components/pages/ItemListContainer/ItemCount/ItemCount.js
+++ src/components/pages/ItemListContainer/ItemCount/ItemCount.js
@@ -9,11 +9,8 @@ function ItemCount({ stock, initial, onAdd }) {
   const decrease = () =>
     counter >= initial ? setCounter(counter - 1) : setCounter(initial);
 
-<<<<<<< HEAD:src/components/ItemCount/ItemCount.js
-  const reset = () => setCounter(initial);
+  const reset = () => setCounter(0);
 
-=======
->>>>>>> secondary:src/components/pages/ItemListContainer/ItemCount/ItemCount.js
   return (
     <>
       <div className="d-flex w-100 jc-between mt-3">
@@ -30,11 +27,7 @@ function ItemCount({ stock, initial, onAdd }) {
         <button
           className="btn-info bg-info bg-secondary py-1 w-25"
           onClick={decrease}
-<<<<<<< HEAD:src/components/ItemCount/ItemCount.js
-          disabled={counter <= initial}
-=======
           disabled={counter <= 0}
->>>>>>> secondary:src/components/pages/ItemListContainer/ItemCount/ItemCount.js
         >
           <i className="fas fa-minus"></i>
         </button>
@@ -42,28 +35,11 @@ function ItemCount({ stock, initial, onAdd }) {
 
       <div className="d-flex jc-between">
         <button
-<<<<<<< HEAD:src/components/ItemCount/ItemCount.js
-          className={
-            counter === 0
-              ? "d-none"
-              : "btn-secundario bg-danger text-white w-25"
-          }
-=======
           className={counter === 0 ? "d-none" : "btn-remove text-white w-25"}
->>>>>>> secondary:src/components/pages/ItemListContainer/ItemCount/ItemCount.js
           onClick={reset}
         >
           <i className="fas fa-trash"></i>
         </button>
-<<<<<<< HEAD:src/components/ItemCount/ItemCount.js
-        <button
-          className="btn-cart w-100 mt-3"
-          onClick={() => onAdd(counter)}
-          disabled={counter === 0}
-        >
-          Agregar al Carrito <i className="fas fa-cart-plus"></i>
-        </button>
-=======
         {
           <button
             className="btn-cart w-100 mt-3"
@@ -73,7 +49,6 @@ function ItemCount({ stock, initial, onAdd }) {
             Agregar al Carrito <i className="fas fa-cart-plus"></i>
           </button>
         }
->>>>>>> secondary:src/components/pages/ItemListContainer/ItemCount/ItemCount.js
       </div>
     </>
   );
