7c40a85be2f248681e9bd1af2ce779fcbe8e9546
Joaquin Arias <joaquinarias989@gmail.com>
2022-06-16 21:26:58 -0300

    Actualizado el .gitignore

--- src/context/cartContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-export const CartContext = createContext([]);
-
-export const CartContextProvider = ({ children }) => {
-  return <CartContext.Provider>{children}</CartContext.Provider>;
-};
